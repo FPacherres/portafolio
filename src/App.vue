9dad5025838e678f74c040849050fd90940e0c1d
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:19:42 -0500

    App Vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Skills from './views/Skills.vue'
+import Studies from './views/Studies.vue'
+import Experience from './views/Experience.vue'
+</script>
+
+<template>
+  <home />
+  <about />
+  <skills />
+  <studies />
+  <experience />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100vh;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
