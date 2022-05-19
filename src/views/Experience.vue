306a76e4b7131face11e05437392f318e0bd1cc9
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:23:22 -0500

    Add view Experience

---
+++ src/views/Experience.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>Experience</section>
+</template>
+
+<script>
+export default {
+  name: 'Experience',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background-color: #362f36;
+}
+</style>
