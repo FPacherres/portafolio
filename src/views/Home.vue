6fe26090ce880a336436f224a0b7b0b34f46d8c2
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:22:38 -0500

    Add view Home

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>Home</section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background-color: #f132f1;
+}
+</style>
