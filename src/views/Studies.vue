27554d53816f8493b23539d9edcc0c12bd1433b3
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:23:13 -0500

    Add view Studies

---
+++ src/views/Studies.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>Studies</section>
+</template>
+
+<script>
+export default {
+  name: 'Studies',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background-color: #644764;
+}
+</style>
