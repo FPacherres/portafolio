dde5a6b4265f3bb9bdbd720cf88ba38762697560
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:22:49 -0500

    Add view About

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>About</section>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background-color: #bf4dbf;
+}
+</style>
