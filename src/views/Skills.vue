71c48e4629cd983684c03751c58fa712f8faece5
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-19 01:23:00 -0500

    Add view Skills

---
+++ src/views/Skills.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>Skills</section>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  background-color: #904f90;
+}
+</style>
