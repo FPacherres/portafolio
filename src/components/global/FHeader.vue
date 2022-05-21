0052d88d253c1f22cde214ec9ab8c3faf2456494
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-20 21:22:43 -0500

    Add changetheme

--- src/components/global/FHeader.vue
+++ src/components/global/FHeader.vue
@@ -8,51 +8,97 @@
       <li>Studies</li>
       <li>Experience</li>
     </ul>
-    <button>A</button>
+    <button class="btn theme" @click="changeTheme">
+      <SunIcon v-if="icon" class="icon" />
+      <MoonIcon v-else class="icon" />
+    </button>
   </nav>
 </template>
 
 <script>
+import { MoonIcon, SunIcon } from '@heroicons/vue/solid'
 export default {
   name: 'FHeader',
-  components: {},
+  components: { MoonIcon, SunIcon },
   data() {
-    return {}
+    return {
+      colorScheme: matchMedia('(prefers-color-scheme: dark)').matches,
+      icon: true
+    }
+  },
+  methods: {
+    changeTheme() {
+      let newStyle = document.documentElement.style
+      if (!this.colorScheme) {
+         newStyle.setProperty('--bg-1', '#fac20b')
+        newStyle.setProperty('--bg-2', '#ef0070')
+        newStyle.setProperty('--bg-3', '#edf1fd')
+        newStyle.setProperty('--bg-4', '#b3b3b3')
+        newStyle.setProperty('--bg-5', '#404855')
+        newStyle.setProperty('--shadow', '#323842 3px 3px 6px 0px, #4c535d -3px -3px 6px 1px')
+        this.icon = true
+      } else {
+        newStyle.setProperty('--bg-1', '#ef0070')
+        newStyle.setProperty('--bg-2', '#fac20b')
+        newStyle.setProperty('--bg-3', '#404855')
+        newStyle.setProperty('--bg-4', '#b3b3b3')
+        newStyle.setProperty('--bg-5', '#edf1fd')
+        newStyle.setProperty('--shadow', 'rgb(204, 219, 232) 3px 3px 6px 0px, rgba(255, 255, 255, 0.8) -3px -3px 6px 1px')
+        this.icon = false
+      }
+      this.colorScheme = !this.colorScheme
+    },
   },
 }
 </script>
 
 <style scoped>
 nav {
-    background-color: #EDF1FD;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 80px;
+  background-color: var(--bg-5);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
 }
 img {
-    width: 40px;
-    height: auto;
+  width: 40px;
+  height: auto;
 }
 ul {
-    width: 550px;
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-    text-transform: uppercase;
-    font-size: 14px;
-    font-weight: 300;
-    transition: .4s;
+  width: 550px;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: 300;
+  transition: 0.4s;
+  color: var(--bg-3);
 }
 li:nth-child(1) {
-    color: #EF0070;
+  color: var(--bg-1);
 }
 li {
-    width: 100%;
-    cursor: pointer;
+  width: 100%;
+  cursor: pointer;
 }
 li:hover {
-    color: #EF0070;
-    transition: .4s;
+  color: var(--bg-1);
+  transition: 0.4s;
+}
+.icon {
+  width: 20px;
+  height: 20px;
+  color: var(--bg-3);
+  margin: -1px 0 0 1.5px;
+}
+.theme {
+  border-radius: 50%;
+  box-shadow: var(--shadow);
+  height: 30px;
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
