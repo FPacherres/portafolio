0a8f09fecd4b49983454e6201a2a7a8964ce321b
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-20 21:23:02 -0500

    Theme dark and light

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Skills from './views/Skills.vue'
-import Studies from './views/Studies.vue'
-import Experience from './views/Experience.vue'
-import FHeader from './components/global/FHeader.vue'
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Skills from "./views/Skills.vue";
+import Studies from "./views/Studies.vue";
+import Experience from "./views/Experience.vue";
+import FHeader from "./components/global/FHeader.vue";
 </script>
 
 <template>
@@ -19,19 +19,19 @@ import FHeader from './components/global/FHeader.vue'
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
 #app {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--bg-3);
   width: 100%;
   height: 100vh;
 }
 body {
   margin: 0px;
-  background-color: #EDF1FD;
+  background-color: var(--bg-5);
 }
 .container {
   width: 100%;
@@ -39,7 +39,7 @@ body {
   flex-direction: column;
   align-items: center;
 }
-#app{
+#app {
   overflow-y: auto;
 }
 #app::-webkit-scrollbar {
@@ -48,7 +48,7 @@ body {
 #app::-webkit-scrollbar-thumb {
   width: 5px;
   border-radius: 4px;
-  background-color: #EF0070;
+  background-color: var(--bg-1);
 }
 .is-full-width {
   width: 1000px;
@@ -61,5 +61,35 @@ body {
 }
 .absolute {
   position: absolute;
+}
+.btn {
+  outline: none;
+  cursor: pointer;
+  border: none;
+  background: transparent;
+}
+
+html {
+  --bg-1: #ef0070;
+  --bg-2: #fac20b;
+  --bg-3: #404855;
+  --bg-4: #b3b3b3;
+  --bg-5: #edf1fd;
+  --shadow: rgb(204, 219, 232) 3px 3px 6px 0px,
+    rgba(255, 255, 255, 0.8) -3px -3px 6px 1px;
+}
+
+@media (prefers-color-scheme: dark) {
+  html {
+    --white: #ffffff;
+
+    --bg-1: #fac20b;
+    --bg-2: #ef0070;
+    --bg-3: #edf1fd;
+    --bg-4: #b3b3b3;
+    --bg-5: #404855;
+    --shadow: #323842 3px 3px 6px 0px,
+      #4c535d -3px -3px 6px 1px;
+  }
 }
 </style>
