d29950b505acdc667e3a07001630b77b6284fa5a
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-23 23:57:37 -0500

    Add girhbu icons snippet user

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,17 +3,41 @@
     <div class="containerDescription">About</div>
     <div class="containerSnippet">
       <Avatar class="avatar" />
+      <div class="github">
+        <div class="relative circle shadow">
+          <h1>Repositories</h1>
+          <h1>43</h1>
+          <div class="tag circle absolute shadow">
+            <fa class="icon" :icon="['fa', 'book-bookmark']" />
+          </div>
+        </div>
+        <div class="relative circle shadow">
+          <h1>Projects</h1>
+          <h1>1</h1>
+          <div class="tag circle absolute shadow">
+            <fa class="icon" :icon="['fa', 'table']" />
+          </div>
+        </div>
+        <div class="relative circle shadow">
+          <h1>Stars</h1>
+          <h1>17</h1>
+          <div class="tag circle absolute shadow">
+           <StarIcon class="icon star" />
+          </div>
+        </div>
+      </div>
     </div>
     <div class="containerCV">About</div>
   </section>
 </template>
 
 <script>
+import { StarIcon } from '@heroicons/vue/outline'
 import Avatar from "../components/global/Avatar.vue";
 export default {
   name: 'About',
   components: { 
-    Avatar
+    Avatar, StarIcon
   },
   data() {
     return {}
@@ -28,7 +52,7 @@ section {
   border: 1px solid #f132f1;
   display: grid;
   grid-template-columns: 1fr 300px;
-  grid-template-rows: 1fr 60px;
+  grid-template-rows: 1fr 120px;
   grid-template-areas: "description snippet" "cv cv"; 
 }
 .containerDescription {
@@ -38,6 +62,13 @@ section {
 .containerSnippet {
   grid-area: snippet;
   border: 1px solid #000000;
+  /* align-self: flex-end;
+  justify-self: center; */
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 50px;
 }
 .containerCV {
   grid-area: cv;
@@ -51,6 +82,37 @@ section {
   width: 200px;
   height: auto;
   margin: 0 30px -120px 0;
+}
+.github {
+  display: flex;
+  gap: 15px;
+}
+.github div {
+  width: 90px;
+  height: 90px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.github div h1 {
+  font-weight: 400;
+  font-size: 12px;
+}
+.github div .tag  {
+  width: 30px;
+  height: 30px;
+  background-color: var(--bg-5);
+  bottom: 0;
+  right: 0;
+}
+.github div .tag .icon {
+  font-size: 10px;
+  color: var(--bg-2);
+}
+.github div .tag .star{
+  width: 11px;
+  height: 11px;
 }
 @media (max-width: 870px) {
   section {
