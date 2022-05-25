03004982d86b164ddfeadf7bc144fda76e62c753
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-24 23:49:25 -0500

    struct view skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,13 +1,26 @@
 <template>
   <section>
-    <div class="cont">Skills</div>
+    <Title :num="'02'" :text="'Skills'" />
+    <div class="container">
+      <div class="card relative">
+        <div class="canva"></div>
+        <div class="title"></div>
+        <div class="list"></div>
+      </div>
+      <div class="card relative">
+        <div class="canva"></div>
+        <div class="title"></div>
+        <div class="list"></div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import Title from "../components/global/Title.vue";
 export default {
   name: 'Skills',
-  components: {},
+  components: { Title },
   data() {
     return {}
   },
@@ -19,8 +32,37 @@ section {
   padding: 80px 0 60px;
   box-sizing: border-box;
 }
-.cont {
-  border: 1px solid #f132f1;
+.container {
+  height: calc(100% - 160px);
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1px;
+}
+.card {
   height: 100%;
+  display: grid;
+  grid-template-rows: 1fr 40px 80px;
+  gap: 1px;
+}
+.canva {
+  background: rgba(255, 255, 255, 0.177);
+}
+.title {
+  background: rgba(255, 255, 255, 0.177);
+}
+.list {
+  background: rgba(255, 255, 255, 0.177);
+}
+@media (max-width: 870px) {
+  section {
+    padding: 80px 0 20px;
+  }
+  .container {
+    grid-template-columns: 1fr;
+  }
+  .card:nth-child(2){
+    display: none;
+  }
 }
 </style>
