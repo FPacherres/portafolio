03bad5fd4af4c948f1e9f13969d8bebd372c8e77
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-28 17:19:45 -0500

    style inpunt basic form

--- src/components/global/FForm.vue
+++ src/components/global/FForm.vue
@@ -3,7 +3,8 @@
     <h1>Hablemos!</h1>
     <input type="text" class="shadow-total" />
     <input type="text" class="shadow-total" />
-    <textarea />
+    <textarea class="shadow-total" />
+    <button class="btn send shadow">Enviar</button>
   </form>
 </template>
 
@@ -27,12 +28,12 @@ form {
 }
 h1 {
   color: var(--bg-6);
-  font-weight: 500;
+  font-weight: 600;
   font-size: 30px;
 }
 input {
-  border-radius: 25px;
-  height: 40px;
+  border-radius: 30px;
+  height: 55px;
   border: 3px var(--bg-5) solid;
   background-color: var(--bg-5);
   width: 300px;
@@ -40,6 +41,30 @@ input {
   box-sizing: border-box;
   color: var(--bg-3);
   font-weight: 200;
+}
+.send {
+    padding: 10px 0;
+    border-radius: 30px;
+    background-color: var(--bg-5);
+    color: var(--bg-6);
+    font-size: 20px;
+    font-weight: 500;
+    transition: .3s;
+}
+.send:hover {
+    box-shadow: var(--shadow-inset);
+    transition: .3s;
+}
+textarea {
+    border-radius: 25px;
+    border: 3px var(--bg-5) solid;
+    background-color: var(--bg-5);
+    height: 60px;
+    padding: 0px 10px;
+    box-sizing: border-box;
+    color: var(--bg-3);
+    font-weight: 200;
+    
 }
 @media (max-width: 870px) {
   h1 {
@@ -49,6 +74,9 @@ input {
     width: 240px;
     height: 50px;
     border: 4px var(--bg-5) solid;
+  }
+  .send {
+      font-size: 18px;
   }
 }
 </style>
