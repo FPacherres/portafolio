c9d7234546b6a808678ee0fe057cb5c927b4bfc4
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-28 16:55:57 -0500

    add component footer

---
+++ src/components/global/FFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <span>© FabianXRL16, 2022 All rights reserved.</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    components: {},
+    data() {
+        return {}
+    },
+}
+</script>
+
+<style scoped>
+div {
+    padding: 0px;
+    color: var(--bg-6);
+    /* padding-top: 16px; */
+    font-size: 12px;
+}
+
+@media (max-width: 870px) {
+    div {
+        font-size: 10px;
+    }
+}
+</style>
