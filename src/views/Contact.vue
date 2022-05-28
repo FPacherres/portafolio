430aeb9c09616fcdacbfbe40d770fec3c9acfe1a
FabianXRL16 <fabianpacherres@gmail.com>
2022-05-28 11:34:18 -0500

    created view contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div class="cont">Contact</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+section {
+  padding: 80px 0 60px;
+  box-sizing: border-box;
+}
+.cont {
+  border: 1px solid #f132f1;
+  height: 100%;
+}
+</style>
