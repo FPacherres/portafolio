4e7e5be24e1bf0d1893354ce90ca1f2f4560c5a4
FabianXRL16 <fabianpacherres@gmail.com>
2022-06-02 21:14:41 -0500

    add data form json

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,44 +1,45 @@
 <template>
-  <section>
+  <div class="section">
     <Title :num="'05'" :text="'Experience'" />
-    <div class="container">
-      <Card />
+    <div class="cardList">
+      <Card class="box" v-for="project in projects" :data="project" :key="project" />
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
 import Title from '../components/global/Title.vue'
 import Card from '../components/global/Card.vue'
+import data from '../Api/projects.json'
 export default {
   name: 'Experience',
   components: { Title, Card },
   data() {
-    return {}
-  },
+    return {
+      projects: data,
+    }
+  }
 }
 </script>
 
 <style scoped>
-section {
-  padding: 80px 0 60px;
+.section {
+  padding: 80px 0 0;
   box-sizing: border-box;
+  display: grid;
+  grid-template-rows: 131.8px 470px;
+  gap: 30px;
 }
-
-.container {
-  border: 1px solid #f132f1;
-  height: calc(100vh - 300px);
-  margin-top: 40px;
+.cardList {
+  overflow: auto;
+  white-space: nowrap;
+  box-sizing: border-box;
+  gap: 10px;
 }
-
+.box {
+  display: inline-block;
+  margin-left: 10px;
+}
 @media (max-width: 870px) {
-  section {
-    padding: 80px 0 20px;
-  }
-  .container {
-    border: 1px solid #f132f1;
-    height: calc(100vh - 260px);
-    margin-top: 40px;
-  }
 }
 </style>
