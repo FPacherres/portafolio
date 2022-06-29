c261244f2309a5b6559bc27c5efd90d359f24b99
FabianXRL16 <fabianpacherres@gmail.com>
2022-06-28 19:11:04 -0500

    add vue-writer to main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueWriter from "vue-writer";
 import App from './App.vue'
 
 library.add(fas, fab)
 
 createApp(App)
+    .use(VueWriter)
     .component('fa', FontAwesomeIcon)
     .mount('#app')
